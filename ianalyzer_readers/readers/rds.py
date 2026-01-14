a3c4602ac67a6485aa8130ff82b473c4c6f19a8e
Luka van der Plas <lukavdplas@mailfence.com>
2026-01-14 17:35:37 +0100

    add docstring to rds reader

--- ianalyzer_readers/readers/rds.py
+++ ianalyzer_readers/readers/rds.py
@@ -7,16 +7,22 @@ from .core import Reader
 
 
 class RDSReader(Reader):
+    '''
+    A base class for Readers that extract data from RDS files (containing serialised
+    R dataframes).
+
+    RDS files are parsed using the `pyreadr` library.
+
+    Only file sources are supported. Row values can be extracted with the CSV extractor.
+    '''
+
     def data_from_file(self, path) -> Iterable[Dict]:
         result = pyreadr.read_r(path)
         data: pandas.DataFrame = result['data']
 
         for _, row in data.iterrows():
-            yield {
-                index: value
-                for index, value in row.items()
-            }
+            yield {index: value for index, value in row.items()}
 
     def iterate_data(self, data: Iterable[Dict], metadata):
         for row in data:
-            yield {'rows': [row]} # this format is for compatability with the CSV reader
+            yield {'rows': [row]} # format is for compatability with the CSV extractor
